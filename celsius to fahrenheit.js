4252108cc3d532328cf47b83a2c9052e1f0b6aa1
Sadimn02 <162736086+Sadimn02@users.noreply.github.com>
2024-10-06 20:21:09 +0600

    Add files via upload

---
+++ celsius to fahrenheit.js
@@ -0,0 +1,7 @@
+function convert(x) {
+    let fahrenhit = (x * 9 / 5) + 32;
+    return fahrenhit;
+}
+let celsius = parseInt(prompt("take s user input of celsius: "));
+
+document.write("fahrenhit is : ", convert(celsius));
